70c1f2d24eed152b0a49a9a8c5e40091486ed2d0
RROSANN <74143411+RROSANN@users.noreply.github.com>
2021-01-30 21:30:14 +0200

    adding db conection like in the heroku thing

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-closed-70780',
-    user : 'postgres',
-    password : 'test',
-    database : 'facerecognition'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
